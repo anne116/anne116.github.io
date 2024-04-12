776c3c3c43a828849241e73be66ce6b708a0c7a5
anne116 <yianchen116@gmail.com>
2024-04-12 23:03:19 +0800

    Add my node.js project.
    Completed the first assignment of the third week. Write a simple web server program and start it(http://localhost:3000/)

--- remote-assignments/Week-3/my-node-project/app.js
+++ remote-assignments/Week-3/my-node-project/app.js
@@ -1,1 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
 
+//Define a route
+app.get('/', (req, res) => {
+ res.send('<h1>Hello, My Sever! Let\'s kick of the third week!</h1>');
+});
+
+//Start the server
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:3000/`);
+});
